4d6e5a3dcf61367e4e1fbe9750cccf334f68aafe
Juliana <julianapereira.dev@gmail.com>
2023-07-03 19:45:39 -0300

    Requisito POST /participants

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {MongoClient} from "mongodb"
+import { MongoClient } from "mongodb";
+import Joi from "joi";
+import dayjs from "dayjs";
 
 const app = express();
 
@@ -11,23 +13,60 @@ app.use(express.json());
 dotenv.config();
 
 //Conexão DB:
-const mongoClient = new MongoClient(process.env.DATABASE_URL)
-try{
-mongoClient.connect()
-console.log("MongoDB Conectado!")
-}
-catch(err) {
-    console.log(err.message)
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+try {
+  mongoClient.connect();
+  console.log("MongoDB Conectado!");
+} catch (err) {
+  console.log(err.message);
 }
 
-const db = mongoClient.db()
-
+const db = mongoClient.db();
 
 //Schemas:
+const participantSchema = Joi.object({ name: Joi.string().required() });
 
 // Funções (endpoints):
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
 
+  const validation = participantSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  if (validation.error) {
+    return res
+      .status(422)
+      .send(validation.error.details.map((detail) => detail.message));
+  }
+
+  try {
+    const participant = await db.collection("participants").findOne({ name });
+
+    if (participant) {
+      return res.sendStatus(409);
+    }
+    
+    const timestamp = Date.now();
+    await db
+      .collection("participants")
+      .insertOne({ name, lastStatus: timestamp });
+
+      const message = {
+        from: name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: dayjs(timestamp).format("HH:mm:ss"),
+    };
+
+    await db.collection("messages").insertOne(message)
+    res.sendStatus(201)
+    
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 // Ligar a aplicação do servidos para ouvir as requisições:
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
